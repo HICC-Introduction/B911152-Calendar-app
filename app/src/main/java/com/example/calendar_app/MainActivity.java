e9d9ca2069b9033b34eb39daf6d52ade418b0ccc
lcy923 <lcy923@naver.com>
2021-03-31 01:13:43 +0900

    upload

---
+++ app/src/main/java/com/example/calendar_app/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.calendar_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        //xml file  에서 만든거
+        final TextView selectedDay = findViewById(R.id.selectedDay);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange( CalendarView view, int year, int month, int dayOfMonth) {
+                selectedDay.setText(year + "년 " + (month+1) + "월 " + dayOfMonth + "일"); // 선택된 날짜가 위에 중간에 뜨도록
+            }
+        });
+
+        final EditText textInput = findViewById(R.id.textInput);
+
+    }
+
+}
