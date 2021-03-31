6fa729ee981e855427d95aa7033d6024cdfe2318
lcy923 <lcy923@naver.com>
2021-04-01 02:49:26 +0900

    텍스트 저장기능까지 구현

--- app/src/main/java/com/example/calendar_app/MainActivity.java
+++ app/src/main/java/com/example/calendar_app/MainActivity.java
@@ -2,29 +2,61 @@ package com.example.calendar_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+private int currentYear = 0;
+private int currentMonth = 0;
+private int currentDay = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CalendarView calendarView = findViewById(R.id.calendarView);
-        //xml file  에서 만든거
+        final CalendarView calendarView = findViewById(R.id.calendarView);
         final TextView selectedDay = findViewById(R.id.selectedDay);
+
+        final List<String> calendarStrings = new ArrayList<>();
+        int[] Days = new int[30];
+        final EditText textInput = findViewById(R.id.textInput);
+
+        final View dayContent = findViewById(R.id.dayContent);
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange( CalendarView view, int year, int month, int dayOfMonth) {
-                selectedDay.setText(year + "년 " + (month+1) + "월 " + dayOfMonth + "일"); // 선택된 날짜가 위에 중간에 뜨도록
+                selectedDay.setText(year + "년 " + (month+1) + "월 " + dayOfMonth + "일");
+                // 선택된 날짜가 위에 중간에 뜨도록
+                currentYear = year;
+                currentMonth = month+1;
+                currentDay = dayOfMonth;
+                /*long saveDate = Long.parseLong(calendarStrings.get(0));
+                if(view.getDate() == saveDate){
+                    textInput.setText(calendarStrings.get(1));
+                }*/
+                if(dayContent.getVisibility()==View.GONE){
+                    dayContent.setVisibility(View.VISIBLE);
+                }
+                if(currentDay==Days[0]){
+                    textInput.setText(calendarStrings.get(0));
+                }
             }
         });
 
-        final EditText textInput = findViewById(R.id.textInput);
+        final Button saveTextButton = findViewById(R.id.saveTextButton);
 
+        saveTextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Days[0] = currentDay;
+                calendarStrings.add(textInput.getText().toString());
+                textInput.setText("");
+            }
+        });
     }
-
 }
