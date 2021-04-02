422aae3792bd06ab3ed39b4876eee9cc0cf1bd0c
lcy923 <lcy923@naver.com>
2021-04-03 02:14:53 +0900

    문제점 : 그날 기록한게 저장한 후 다른 날짜에도 뜨는 것 -> 수정후 문제점 : 저장은 되는데 그 상태로 다른 날짜에 가면 사라짐 -> 수정 후 문제점 : 같은 날짜의 다른 년도에 같은 일정이 뜸

--- app/src/main/java/com/example/calendar_app/MainActivity.java
+++ app/src/main/java/com/example/calendar_app/MainActivity.java
@@ -15,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
 private int currentYear = 0;
 private int currentMonth = 0;
 private int currentDay = 0;
+
+private int Index = 0;
+//private int monthIndex = 0;
+//private int yearIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +27,12 @@ private int currentDay = 0;
         final TextView selectedDay = findViewById(R.id.selectedDay);
 
         final List<String> calendarStrings = new ArrayList<>();
-        int[] Days = new int[30];
+
+        final int numberOfDays = 2000;
+        final int[] days = new int[numberOfDays];
+        final int[] months = new int[numberOfDays];
+        final int[] years = new int[numberOfDays];
+
         final EditText textInput = findViewById(R.id.textInput);
 
         final View dayContent = findViewById(R.id.dayContent);
@@ -34,7 +43,7 @@ private int currentDay = 0;
                 selectedDay.setText(year + "년 " + (month+1) + "월 " + dayOfMonth + "일");
                 // 선택된 날짜가 위에 중간에 뜨도록
                 currentYear = year;
-                currentMonth = month+1;
+                currentMonth = month;
                 currentDay = dayOfMonth;
                 /*long saveDate = Long.parseLong(calendarStrings.get(0));
                 if(view.getDate() == saveDate){
@@ -43,20 +52,35 @@ private int currentDay = 0;
                 if(dayContent.getVisibility()==View.GONE){
                     dayContent.setVisibility(View.VISIBLE);
                 }
-                if(currentDay==Days[0]){
-                    textInput.setText(calendarStrings.get(0));
+                for(int i =0; i<30; i++){
+                    if(days[i] == currentDay){
+                        for(int j=0;j<12;j++){
+                            if(months[j] == currentMonth&&days[j]==currentDay){
+                                textInput.setText(calendarStrings.get(i));
+                                return;
+                            }
+                        }
+                    }
                 }
+                textInput.setText("");
             }
         });
 
         final Button saveTextButton = findViewById(R.id.saveTextButton);
 
-        saveTextButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Days[0] = currentDay;
-                calendarStrings.add(textInput.getText().toString());
+        saveTextButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                days[Index] = currentDay;
+                months[Index] = currentMonth;
+                years[Index] = currentYear;
+                calendarStrings.add(Index, textInput.getText().toString());
+
+               // monthIndex++;
+                //yearIndex++;
                 textInput.setText("");
+                Index++;
             }
         });
+
     }
 }
