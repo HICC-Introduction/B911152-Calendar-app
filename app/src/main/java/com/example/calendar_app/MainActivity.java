a08f962bc8a58d787947133916f01182c6683df7
lcy923 <lcy923@naver.com>
2021-04-04 01:20:10 +0900

    텍스트 작성 부분 공간을 늘리고 줄바꿈이 가능하게 만들었다. 날짜를 표시해 주던 부분을 오늘 날짜라 돌아올 수 있도록 하는 버튼으로 바꾸었다

--- app/src/main/java/com/example/calendar_app/MainActivity.java
+++ app/src/main/java/com/example/calendar_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.calendar_app;
 
 import android.content.Context;
+import android.icu.util.Output;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,16 +11,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     private int currentYear = 0;
     private int currentMonth = 0;
     private int currentDay = 0;
@@ -30,40 +36,31 @@ public class MainActivity extends AppCompatActivity {
     private int[] days;
     private int[] months;
     private int[] years;
-    //private int monthIndex = 0;
-//private int yearIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final CalendarView calendarView = findViewById(R.id.calendarView);
-        final TextView selectedDay = findViewById(R.id.selectedDay);
 
         calendarStrings = new ArrayList<>();
-
-        final int numberOfDays = 3000;
+        final int numberOfDays = 2000;
         days = new int[numberOfDays];
         months = new int[numberOfDays];
         years = new int[numberOfDays];
 
         readInfo();
-
         final EditText textInput = findViewById(R.id.textInput);
-
         final View dayContent = findViewById(R.id.dayContent);
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                selectedDay.setText(year + "년 " + (month + 1) + "월 " + dayOfMonth + "일");
-                // 선택된 날짜가 위에 중간에 뜨도록
+                //selectedDay.setText(year + "년 " + (month + 1) + "월 " + dayOfMonth + "일");
+
                 currentYear = year;
                 currentMonth = month;
                 currentDay = dayOfMonth;
-                /*long saveDate = Long.parseLong(calendarStrings.get(0));
-                if(view.getDate() == saveDate){
-                    textInput.setText(calendarStrings.get(1));
-                }*/
                 if (dayContent.getVisibility() == View.GONE) {
                     dayContent.setVisibility(View.VISIBLE);
                 }
@@ -99,41 +96,88 @@ public class MainActivity extends AppCompatActivity {
                 dayContent.setVisibility(View.GONE);
             }
         });
-
+        final Button todayButton = findViewById(R.id.todayButton);
+        todayButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                calendarView.setDate(calendarView.getDate());
+            }
+        });
         }
         @Override
         protected void onPause(){
             super.onPause();
-            try {
-                saveInfo();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            saveInfo();
         }
-        private void saveInfo() throws IOException {
-            File file = new File(this.getFilesDir(), "calendarStrings");
+        private void saveInfo() {
+            File file = new File(this.getFilesDir(), "saved");
+            File daysFile = new File(this.getFilesDir(),"days");
+            File monthsFile = new File(this.getFilesDir(),"months");
+            File yearsFile = new File(this.getFilesDir(),"years");
+
             try{
-                FileWriter fileWriter = new FileWriter("calendarStrings");
-                final int calendarStringsCount = calendarStrings.size();
-                for (int i =0; i<calendarStringsCount; i++){
-                    fileWriter.write(calendarStrings.get(i));
+                FileOutputStream fOut = new FileOutputStream(file);
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fOut));
+
+                FileOutputStream fOutDays = new FileOutputStream(daysFile);
+                BufferedWriter bwDays = new BufferedWriter(new OutputStreamWriter(fOut));
+
+                FileOutputStream fOutMonths = new FileOutputStream(monthsFile);
+                BufferedWriter bwMonths = new BufferedWriter(new OutputStreamWriter(fOut));
+
+                FileOutputStream fOutYears = new FileOutputStream(yearsFile);
+                BufferedWriter bwYears = new BufferedWriter(new OutputStreamWriter(fOut));
+
+                for(int i=0; i<Index; i++){
+                    bw.write(calendarStrings.get(i));
+                    bw.newLine();
+                    bwDays.write(days[i]);
+                    bwMonths.write(months[i]);
+                    bwYears.write(years[i]);
                 }
-                fileWriter.close();
-            }catch(IOException e){
+                bw.close();
+                fOut.close();
+                bwDays.close();
+                fOutDays.close();
+                bwMonths.close();
+                fOutMonths.close();
+                bwYears.close();
+                fOutYears.close();
+            }catch(Exception e){
                 e.printStackTrace();
             }
     }
     private void readInfo(){
+        File file = new File(this.getFilesDir(), "saved");
+        File dayFile = new File(this.getFilesDir(),"days");
+        File monthsFile = new File(this.getFilesDir(),"months");
+        File yearsFile = new File(this.getFilesDir(),"years");
+
+        if(!file.exists()){
+            return;
+        }
         try{
-            BufferedReader reader = new BufferedReader(new FileReader("calendarStrings"));
-            String line = null;
-            while ((line = reader.readLine()) != null){
+            FileInputStream is = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            FileInputStream isDays = new FileInputStream(dayFile);
+            BufferedReader readerDays = new BufferedReader(new InputStreamReader(isDays));
+            FileInputStream isMonths = new FileInputStream(monthsFile);
+            BufferedReader readerMonths = new BufferedReader(new InputStreamReader(isMonths));
+            FileInputStream isYears = new FileInputStream(yearsFile);
+            BufferedReader readerYears = new BufferedReader(new InputStreamReader(isYears));
+
+            int i =0;
+            String line = reader.readLine();
+
+            while (line != null){
                 calendarStrings.add(line);
+                line = reader.readLine();
+                days[i]=readerDays.read();
+                months[i]=readerMonths.read();
+                years[i]=readerYears.read();
+                i++;
             }
-            reader.close();
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-        }catch (IOException e){
+            Index = i;
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
